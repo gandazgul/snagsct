b5dda39b76df9074351afe07865e30542e701e59
Carlos Ravelo <ravelo.carlos@gmail.com>
2022-06-19 14:26:12 -0400

    Added the rest of the files

---
+++ queue/src/App.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { firebase } from './firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import {
+    createTheme,
+    ThemeProvider,
+    CssBaseline,
+    Box,
+} from '@mui/material';
+import ResponsiveAppBar from './AppBar';
+import Queue from './Queue';
+
+import './App.css';
+
+// Create a theme instance.
+const theme = createTheme({
+    typography: {
+        gameTitle: {
+            fontSize: '1rem',
+        },
+        gameDescription: {
+            fontSize: '0.8rem',
+        },
+    },
+    palette: {
+        // mode: 'dark',
+        background: {
+            main: '#f2e5e8',
+        },
+        primary: {
+            main: '#800020',
+        },
+        secondary: {
+            main: '#008060',
+        },
+        error: {
+            main: '#cd0033',
+        },
+    },
+});
+
+function App() {
+    const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+
+    // Configure FirebaseUI.
+    const uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+            // Avoid redirects after sign-in.
+            signInSuccessWithAuthResult: () => false,
+        },
+    };
+
+    // Listen to the Firebase Auth state and set the local state.
+    useEffect(() => {
+        const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+            setIsSignedIn(!!user);
+        });
+        return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+    }, [isSignedIn]);
+
+    function handleSignOut() {
+        firebase.auth().signOut();
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <ResponsiveAppBar user={firebase.auth().currentUser} handleSignOut={handleSignOut} />
+            {
+                isSignedIn ?
+                <Queue /> : (
+                    <Box style={{ padding: '1rem' }}>
+                        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+                    </Box>
+                )
+            }
+        </ThemeProvider>
+    );
+}
+
+export default App;
